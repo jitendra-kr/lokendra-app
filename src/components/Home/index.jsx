f4795da445738efafb7848d557fdc277ffeb0336
Jimmylogic <Jitendrarajput588@gmail.com>
2019-08-10 12:10:15 +0530

    removed banner

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,90 +1,23 @@
 import React from 'react';
-import Banner from './Banner.jsx';
+import ReactDOM from 'react-dom';
+import MainFooter from '../Footer';
+import MainHeader from '../Header';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content } = Layout;
 
 class Home extends React.Component {
   render() {
-    return (
-      <div className="home-page">
-
-        <Banner appName="JP" />
-
-
-        <div className="container ">
-          <div className="row justify-content-md-center">
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/bitcoin-coin.jpg" alt="jimmy" />
-              <p>
-              10,000 American Cryptocurrency Owners Will Receive Warning Letters From the IRS
-              </p>
-            </div>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/army.jpg" alt="jimmy" />
-              <p>
-              The Indian Army is the land-based branch and the largest component of Indian Armed
-              </p>
-            </div>
-          </div>
-          <div className="row justify-content-md-center" style={{"marginTop": "110px"}}>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/uber.jpeg" alt="jimmy" />
-              <p>
-              Uber Technologies, Inc. is an American multinational transportation network company.
-              </p>
-            </div>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/blog.jpg" alt="jimmy" />
-              <p>
-              Creating blog content that is valuable, helpful, and/or entertaining is an effective way to grow.
-              </p>
-            </div>
-          </div>
-          <div className="row justify-content-md-center" style={{"marginTop": "110px"}}>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/mark_zukerberg.jpg" alt="jimmy" />
-              <p>
-              10,000 American Cryptocurrency Owners Will Receive Warning Letters From the IRS
-              </p>
-            </div>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/two.jpeg" alt="jimmy" />
-              <p>
-              Mark Elliot Zuckerberg is an American technology entrepreneur and philanthropist.
-              </p>
-            </div>
-          </div>
-          <div className="row justify-content-md-center" style={{"marginTop": "110px"}}>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/software.jpg" alt="jimmy" />
-              <p>
-              Computer software, or simply software, is a collection of data or compute
-              </p>
-            </div>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/Ramdev.jpg" alt="jimmy" />
-              <p>
-              Baba Ramdev is an Indian yoga guru known for his work in ayurveda
-              </p>
-            </div>
-          </div>
-          <div className="row justify-content-md-center" style={{"marginTop": "110px"}}>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/modi.jpg" alt="jimmy" />
-              <p>
-              Modi is an politician serving as the 14th and current Prime Minister of India since 2014.
-              </p>
-            </div>
-            <div className="col col-lg-4">
-              <img style={{width:'100%', height: "100%"}} src="images/gold.jpg" alt="jimmy" />
-              <p>
-              The No. 1 gold price site for fast loading live gold price charts in ounces.
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    );
+    return ReactDOM.render(
+      <Layout className="layout">
+          <MainHeader appName="JP" />
+          <Content style={{ padding: '0 50px' }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          </Content>
+          <MainFooter appName="JP" />
+        </Layout>,
+          document.getElementById('root'),
+        );
   }
 }
 
