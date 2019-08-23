b7fc29745fb788336dd0232ac2350f8ca12a6a7e
NG7F294 <jitendra.kumar@soprasteria.com>
2019-08-23 16:58:10 +0530

    seprate review progress added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as Header } from './Header';
 export { default as Home } from './Home';
 export { default as ProductDetail } from './Product/ProductDetail';
 export { default as ProductReview } from './Product/ProductReview';
-export { default as RatingPercentage } from './Product/RatingPercentage';
+export { default as RatingPercentage } from './Product/ProductRatingPercentage';
 export { default as Advertisement } from './Advertisement';
