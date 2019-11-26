6d993d8796d88f94c970acfd38824dc45fb28a90
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2019-11-26 14:15:41 +0530

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
-import { Layout } from 'antd';
+import { Layout } from "antd";
 const { Footer } = Layout;
 
 const MainFooter = ({ appName }) => {
-    return (
-        <Footer >
-        Jimmypoint © 2019. All Rights Reserved
-          </Footer>
-  )
+  return (
+    <Footer>
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="col-lg-4 display-inline-block">
+          <Link to={`/`} style={{ color: "#ffffff" }}>
+          Blogs
+          </Link>
+            </div>
+          <div className="col-lg-4 display-inline-block">
+          <Link to={`/questions`} style={{ color: "#ffffff" }}>
+          Questions
+          </Link>
+            </div>
+          <div className="col-lg-4 display-inline-block">
+          <Link to={`/`} style={{ color: "#ffffff" }}>
+          Account
+          </Link>
+            </div>
+        </div>
+      </div>
+      <div className="m-top-30">Jimmypoint © 2019. All Rights Reserved</div>
+    </Footer>
+  );
 };
 
 export default MainFooter;
