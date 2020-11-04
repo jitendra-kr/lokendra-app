44385f192d83f89bf924c76a43d18380eb3c1735
jitendra <jitendra.kumar@soprasteria.com>
2020-11-04 22:22:01 +0530

    condition added

--- src/components/Questions/QuestionList/index.jsx
+++ src/components/Questions/QuestionList/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Layout, Button } from "antd";
+import { Layout, Button, Tabs } from "antd";
 import { upperFirst } from "lodash";
 import { Link, withRouter } from "react-router-dom";
 import "./index.css";
 import get from "axios";
 import Config from '../../../config/env'
 const { Content } = Layout;
+const { TabPane } = Tabs;
+// const { Search } = Input;
 
 class QuestionList extends React.Component {
   constructor(props) {
@@ -13,12 +15,14 @@ class QuestionList extends React.Component {
 
     this.state = {
       data: [],
+      size: "small",
       config: Config.getData().default
     };
   }
 
-  componentDidMount() {
-    get(`${this.state.config.baseUrl}question/question-list`)
+  componentDidMount(type) {
+
+    get(`${this.state.config.baseUrl}question/question-list?by=${type}`)
       .then(response => {
         this.setState({
           data: response.data.result
@@ -27,12 +31,13 @@ class QuestionList extends React.Component {
       .catch(err => {
         console.log("Error Reading data " + err);
       });
+
   }
 
   calculateTitle = title => {
     const limit = 63;
     if (title.length > limit) {
-      return upperFirst( title.substring(0, limit) )+ "...";
+      return upperFirst(title.substring(0, limit)) + "...";
     }
     return upperFirst(title);
   };
@@ -54,52 +59,102 @@ class QuestionList extends React.Component {
     return `/question/${item.slug}`;
   }
 
+  onTabClick(key) {
+    this.componentDidMount(key)
+  }
+
   render() {
     return (
       <Content style={{ padding: "50px 50px" }}>
         <div className="row">
-          <div className="col-lg-3"></div>
-          <div className="col-lg-6">
+          <div className="col-lg-2"></div>
+          <div className="col-lg-8">
             <div className="row">
-            <Button type="primary" style={{position: "absolute", right: "15px"}}>
-            <Link to={`/ask-question`}>New Questions</Link>
-            </Button>
-              {this.state.data.map((item, i) => {
-                return (
-                  <div
-                    className="col-lg-12 cursor-pointer"
-                    key={i}
-                    onClick={() => {
-                      this.handleClick(item);
-                    }}
-                    style={{ marginTop: "40px" }}
-                  >
-                    <div className="listing border">
+              <Tabs
+              defaultActiveKey="all"
+              onTabClick = { this.onTabClick.bind(this)}
+              tabBarExtraContent={
+              <Button type="primary" style={{ right: "32px" }}>
+                <Link to={`/ask-question`}>New Questions</Link>
+              </Button>}
+              style = {{ width: "100%"}}>
+
+                <TabPane tab="All" key="all" style = {{ left: "19px"}} >
+                {/* <Search size="large" style={{ width: "97%", margin: '0px 0px 0px 14px' }} placeholder="input search text" onSearch={this.onSearch} enterButton /> */}
+                  {this.state.data.map((item, i) => {
+                    return (
                       <div
-                        className="home-page-title"
-                        style={{ marginLeft: "25px" }}
+                        className="col-lg-12 cursor-pointer"
+                        key={i}
+                        onClick={() => {
+                          this.handleClick(item);
+                        }}
+                        style={{ marginTop: "30px" }}
                       >
-                        <Link to={{ pathname: this.detailPageUrl(item) }}>
-                          {this.calculateTitle(item.title)}
-                        </Link>
-                      </div>
-                      <div style={{ marginLeft: "35px" }}>
-                        <p>
-                          <span className="question-by">By - </span>
-                          {upperFirst(item.where_asked)}
-                          <span className="question-by"> To -  </span>
-                          {upperFirst(item.author)}
-                          <span className="question-by"> On -  </span>
+                        <div className="listing border">
+                          <div
+                            className="home-page-title"
+                            style={{ marginLeft: "25px" }}
+                          >
+                            <Link to={{ pathname: this.detailPageUrl(item) }}>
+                              {this.calculateTitle(item.title)}
+                            </Link>
+                          </div>
+                          <div style={{ marginLeft: "35px" }}>
+                            <p>
+                              <span className="question-by">By - </span>
+                              {upperFirst(item.where_asked)}
+                              <span className="question-by"> To -  </span>
+                              {upperFirst(item.author)}
+                              <span className="question-by"> On -  </span>
                           8 January 2019
                           </p>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                );
-              })}
+                    );
+                  })}
+                </TabPane>
+                <TabPane tab="My Questions" key="me">
+                {this.state.data.map((item, i) => {
+                    return (
+                      <div
+                        className="col-lg-12 cursor-pointer"
+                        key={i}
+                        onClick={() => {
+                          this.handleClick(item);
+                        }}
+                        style={{ marginTop: "30px" }}
+                      >
+                        <div className="listing border">
+                          <div
+                            className="home-page-title"
+                            style={{ marginLeft: "25px" }}
+                          >
+                            <Link to={{ pathname: this.detailPageUrl(item) }}>
+                              {this.calculateTitle(item.title)}
+                            </Link>
+                          </div>
+                          <div style={{ marginLeft: "35px" }}>
+                            <p>
+                              <span className="question-by">By - </span>
+                              {upperFirst(item.where_asked)}
+                              <span className="question-by"> To -  </span>
+                              {upperFirst(item.author)}
+                              <span className="question-by"> On -  </span>
+                          8 January 2019
+                          </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </TabPane>
+              </Tabs>
+
             </div>
           </div>
-          <div className="col-lg-3"></div>
+          <div className="col-lg-2"></div>
         </div>
       </Content>
     );
