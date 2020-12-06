b6c70c28a5b29f61b843112b1977bc47cca89116
jitendra <jitendra.kumar@soprasteria.com>
2020-12-06 18:13:53 +0530

    improved meta tags

--- src/components/Head/head.js
+++ src/components/Head/head.js
@@ -1,6 +1,23 @@
 import Head from 'next/head'
+import React, { useEffect, useState } from "react";
 
 export default function AppHead(props) {
+
+    const [author, setAuthor] = useState('Jimmypoint');
+
+    const [url, seturl] = useState('http://www.jimmypoint.com');
+    useEffect(() => {
+        let name = author;
+        if(props.data.author.firstName) {
+            name = props.data.author.firstName;
+        }
+        if (props.data.author.firstName && props.data.author.lastName) {
+            name += ' ' + props.data.author.lastName;
+        }
+        seturl(window.location.href);
+        setAuthor(name)
+      }, []);
+
 
     return <Head>
         <title>{props.data.title || "Jimmypoint"}</title>
@@ -11,15 +28,22 @@ export default function AppHead(props) {
             name="viewport"
             content="width=device-width,minimum-scale=1,initial-scale=1" />
         <meta
+            property="og:type"
+            content="website" />
+        <meta
             property="og:site_name"
-            content="Jimmypoint"/>
+            content="Jimmy Point"/>
+        <meta
+            property="og:image"
+            itemprop="image primaryImageOfPage"
+            content="https://raw.githubusercontent.com/jitendra-kr/jimmy-point-images/master/Jimmypoint.png" />
         <meta
             property="og:type"
             content="article"/>
 
         <meta
             property="og:url"
-            content={props.data.url || "Jimmypoint"}/>
+            content={url }/>
 
         <meta
             property="og:title"
@@ -45,7 +69,7 @@ export default function AppHead(props) {
 
         <meta
             name="author"
-            content={props.data.author || "Jimmypoint"} />
+            content={author} />
 
 
     </Head>;
