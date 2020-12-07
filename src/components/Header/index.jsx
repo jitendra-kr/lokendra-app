9d865233f8431ae15b37ddd9b68554533184a304
jitendra <jitendra.kumar@soprasteria.com>
2020-12-07 11:50:17 +0530

    improved header tas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,18 +11,18 @@ const { Header } = Layout;
 const { useBreakpoint } = Grid;
 
 function MainHeader() {
-  const [visible, setvisible] = useState(false);
   let selectedTab = "3";
-  const router = useRouter();
   let [user, setUser] = useContext(UserContext);
+  const [visible, setvisible] = useState(false);
+
+  const router = useRouter();
+  const { md } = useBreakpoint();
+
+  console.log(md);
 
   if (!user || isEmpty(user)) {
     user = "na";
   }
-
-
-
-  const { md } = useBreakpoint();
 
   const keysMapper = {
     "/": "3",
@@ -69,29 +69,37 @@ function MainHeader() {
     return (
       <Menu
         theme="dark"
-        mode={md ? "horizontal" : "inline"}
+        mode={"horizontal"}
         defaultSelectedKeys={[selectedTab]}
         style={{ lineHeight: "64px", float: "right" }}
-
       >
-        <Menu.Item key="3"        onClick={() => {
-          setvisible(false);
-        }}>
+        <Menu.Item
+          key="3"
+          onClick={() => {
+            setvisible(false);
+          }}
+        >
           <Link href="/" style={{ color: "#ffffff" }}>
             Blogs
           </Link>
         </Menu.Item>
-        <Menu.Item key="2"        onClick={() => {
-          setvisible(false);
-        }}>
+        <Menu.Item
+          key="2"
+          onClick={() => {
+            setvisible(false);
+          }}
+        >
           <Link href="/questions" style={{ color: "#ffffff" }}>
             Questions
           </Link>
         </Menu.Item>
         {user === "na" ? (
-          <Menu.Item key="1"        onClick={() => {
-            setvisible(false);
-          }}>
+          <Menu.Item
+            key="1"
+            onClick={() => {
+              setvisible(false);
+            }}
+          >
             <Link href="/login" style={{ color: "#ffffff" }}>
               Login/Register
             </Link>
@@ -141,7 +149,7 @@ function MainHeader() {
             onClick={showDrawer}
           />
           <Drawer
-            title={`Hello ${user === "na" ? '' : user.firstName}`}
+            title={`Hello ${user === "na" ? "" : user.firstName}`}
             placement={"left"}
             closable={false}
             onClose={onDrawerClose}
