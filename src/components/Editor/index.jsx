8f2510fc9182401d162c40d316b7b1e052031d64
jitendra <jitendra.kumar@soprasteria.com>
2020-12-11 22:25:04 +0530

    custom plugin addded ckeditor

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -35,25 +35,7 @@ class Editor extends React.Component {
             // 'fontSize',
             // 'highlight',
             // 'horizontalLine'
-      ],
-        codeBlock: {
-            languages: [
-              { language: 'plaintext', label: 'Plain text' },
-              { language: 'c', label: 'C' },
-              { language: 'cs', label: 'C#' },
-              { language: 'cpp', label: 'C++' },
-              { language: 'css', label: 'CSS' },
-              { language: 'diff', label: 'Diff' },
-              { language: 'html', label: 'HTML' },
-              { language: 'java', label: 'Java' },
-              { language: 'javascript', label: 'JavaScript' },
-              { language: 'php', label: 'PHP' },
-              { language: 'python', label: 'Python' },
-              { language: 'ruby', label: 'Ruby' },
-              { language: 'typescript', label: 'TypeScript' },
-              { language: 'xml', label: 'XML' }
-            ]
-          }
+      ]
     };
 
      onChange = (event, editor) => {
