a1d4691bc255fe0d23e1e8635869336993bc640d
jitendra <jitendra.kumar@soprasteria.com>
2020-12-12 09:30:05 +0530

    custom plugin addded ckeditor

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+
+
 class Editor extends React.Component {
 
     CKEditor;
@@ -7,35 +10,53 @@ class Editor extends React.Component {
 
     componentDidMount() {
         this.CKEditor = require("@ckeditor/ckeditor5-react");
-        this.Editor = require('ckeditor5-custom-build/build/ckeditor');
+        this.Editor = require('@ckeditor/ckeditor5-custom-build/build/ckeditor');
         this.setState({ isServer: false });
 
     }
 
     editorConfiguration = {
         toolbar: [
-            // 'heading',
-            // '|',
+            'heading',
+            '|',
             'bold',
-            // 'italic',
-            // 'link',
-            // 'bulletedList',
-            // 'numberedList',
-            // '|',
-            // '|',
-            // 'blockQuote',
-            // 'insertTable',
-            // 'undo',
-            // 'redo',
-            // 'codeBlock',
-            // 'fontColor',
-            // 'fontBackgroundColor',
-            // 'fontFamily',
-            // 'underline',
-            // 'fontSize',
-            // 'highlight',
-            // 'horizontalLine'
-      ]
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            '|',
+            'blockQuote',
+            'insertTable',
+            'undo',
+            'redo',
+            'codeBlock',
+            'fontColor',
+            'fontBackgroundColor',
+            'fontFamily',
+            'underline',
+            'fontSize',
+            'highlight',
+            'horizontalLine'
+      ],
+        codeBlock: {
+            languages: [
+              { language: 'plaintext', label: 'Plain text' },
+              { language: 'c', label: 'C' },
+              { language: 'cs', label: 'C#' },
+              { language: 'cpp', label: 'C++' },
+              { language: 'css', label: 'CSS' },
+              { language: 'diff', label: 'Diff' },
+              { language: 'html', label: 'HTML' },
+              { language: 'java', label: 'Java' },
+              { language: 'javascript', label: 'JavaScript' },
+              { language: 'php', label: 'PHP' },
+              { language: 'python', label: 'Python' },
+              { language: 'ruby', label: 'Ruby' },
+              { language: 'typescript', label: 'TypeScript' },
+              { language: 'xml', label: 'XML' }
+            ]
+          }
     };
 
      onChange = (event, editor) => {
