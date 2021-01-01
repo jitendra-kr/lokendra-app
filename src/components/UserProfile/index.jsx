1dea54af4581d931954868ec4cde98ff0ffd0b2d
jitendra <jitendra.kumar@soprasteria.com>
2021-01-01 11:05:19 +0530

    implemeted verifyEmail email

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -8,9 +8,10 @@ import {
   inputFieldsLimit,
   messageLoading,
   messageSuccess,
-  messageError } from "../../utils";
+  messageError,
+} from "../../utils";
 import Link from "next/link";
-import {  } from "../../utils/antd";
+import {} from "../../utils/antd";
 import styles from "../../../styles/UserProfile.module.css";
 const { Content } = Layout;
 
@@ -108,143 +109,142 @@ class UserProfile extends React.Component {
     const key = "verifyEmail";
     messageLoading({ key });
     httpPost({
-      url: '/user/send-email-verification-email'
+      url: "/user/send-email-verification-email",
     })
-    .then((response) => {
-      messageSuccess({ content: response.message, key });
-      console.log(response)
-    })
-    .catch((err) => {
-      messageError({ duration: 2 });
-    });
-  }
+      .then((response) => {
+        messageSuccess({ content: response.message, key });
+        console.log(response);
+      })
+      .catch((err) => {
+        messageError({ duration: 2 });
+      });
+  };
 
   render() {
     if (isEmpty(this.state.userData)) {
-      return <div className = "ant-layout-content" ></div>;
+      return <div className="ant-layout-content"></div>;
     }
     return (
-       <Content className=" content-height"  style={{ textAlign: "center" }}>
-            <Descriptions
-              column={1}
-              extra={
-                <Button
-                  className={this.state.tgbtn ? "visible" : "invisible"}
-                  type="primary"
-                  onClick={() => this.toggleButton()}
-                >
-                  Edit
-                </Button>
-              }
+      <Content className=" content-height" style={{ textAlign: "center" }}>
+        <Descriptions
+          column={1}
+          extra={
+            <Button
+              className={this.state.tgbtn ? "visible" : "invisible"}
+              type="primary"
+              onClick={() => this.toggleButton()}
             >
-              <Descriptions.Item label="First Name" >
-                {this.state.tgbtn ? (
-                  <span className={styles['user-profile-value']}>
-                  {this.state.userData?.firstName}
+              Edit
+            </Button>
+          }
+        >
+          <Descriptions.Item label="First Name">
+            {this.state.tgbtn ? (
+              <span className={styles["user-profile-value"]}>
+                {this.state.userData?.firstName}
+              </span>
+            ) : (
+              <Input
+                allowClear={true}
+                name="firstName"
+                className={styles.input}
+                onChange={this.handleInputChange}
+                maxLength={inputFieldsLimit.firstName}
+                defaultValue={this.state.userData.firstName}
+              />
+            )}
+          </Descriptions.Item>
+          <Descriptions.Item label="Last Name">
+            {this.state.tgbtn ? (
+              <span className={styles["user-profile-value"]}>
+                {this.state.userData?.lastName}
+              </span>
+            ) : (
+              <Input
+                allowClear={true}
+                name="lastName"
+                className={styles.input}
+                onChange={this.handleInputChange}
+                maxLength={inputFieldsLimit.lastName}
+                defaultValue={this.state.userData.lastName}
+              />
+            )}
+          </Descriptions.Item>
+          <Descriptions.Item label="Email">
+            {this.state.tgbtn ? (
+              <div className={styles["user-profile-value"]}>
+                <span style={{ marginRight: "10px" }}>
+                  {this.state.userData?.email}
                 </span>
+                {this.state.userData?.verified ? (
+                  <span style={{ color: "#0B8E05" }}>&#10003;</span>
                 ) : (
-                  <Input
-                    allowClear={true}
-                    name="firstName"
-                    className={styles.input}
-                    onChange={this.handleInputChange}
-                    maxLength={inputFieldsLimit.firstName}
-                    defaultValue={this.state.userData.firstName}
-                  />
-                )}
-              </Descriptions.Item>
-              <Descriptions.Item label="Last Name">
-                {this.state.tgbtn ? (
-                  <span className={styles['user-profile-value']}>
-
-                  {this.state.userData?.lastName}
-                  </span>
-                ) : (
-                  <Input
-                    allowClear={true}
-                    name="lastName"
-                    className={styles.input}
-                    onChange={this.handleInputChange}
-                    maxLength={inputFieldsLimit.lastName}
-                    defaultValue={this.state.userData.lastName}
-                  />
-                )}
-              </Descriptions.Item>
-              <Descriptions.Item label="Email">
-                {this.state.tgbtn ? (
-                   <div className={styles['user-profile-value']}>
-
-                  <span  style = {{marginRight: '20px'}}>
-
-                 { this.state.userData?.email }
-
-                  </span>
-                  {this.state.userData?.verified ? <p>verified</p> :
-                  <a style={{ float: "right", color: '#40A9FF' }} onClick = {this.verifyEmail}>
-                  verify email
-                </a>}
-                   </div>
-                ) : (
-                  <Input
-                    allowClear={true}
-                    className={styles.input}
-                    style = {{marginLeft: '30px'}}
-                    name="email"
-                    onChange={this.handleInputChange}
-                    defaultValue={this.state.userData.email}
-                  />
-                )}
-              </Descriptions.Item>
-              <Descriptions.Item label="Gender">
-                <span className={styles['user-profile-value']}>
-
-                <Radio.Group
-                  name="gender"
-                  onChange={this.handleInputChange}
-                  value={this.state.userData?.gender}
-                  disabled={this.state.tgbtn}
+                  <a
+                    style={{ float: "right", color: "#40A9FF" }}
+                    onClick={this.verifyEmail}
                   >
-                  <Radio value={"m"}>Male</Radio>
-                  <Radio value={"f"}>Female</Radio>
-                  <Radio value={"o"}>Other</Radio>
-                </Radio.Group>
-                  </span>
-              </Descriptions.Item>
+                    verify email
+                  </a>
+                )}
+              </div>
+            ) : (
+              <Input
+                allowClear={true}
+                className={styles.input}
+                style={{ marginLeft: "30px" }}
+                name="email"
+                onChange={this.handleInputChange}
+                defaultValue={this.state.userData.email}
+              />
+            )}
+          </Descriptions.Item>
+          <Descriptions.Item label="Gender">
+            <span className={styles["user-profile-value"]}>
+              <Radio.Group
+                name="gender"
+                onChange={this.handleInputChange}
+                value={this.state.userData?.gender}
+                disabled={this.state.tgbtn}
+              >
+                <Radio value={"m"}>Male</Radio>
+                <Radio value={"f"}>Female</Radio>
+                <Radio value={"o"}>Other</Radio>
+              </Radio.Group>
+            </span>
+          </Descriptions.Item>
 
-              <Descriptions.Item label="Password">
-                <div className={styles['user-profile-value']}>
-                  <span style = {{marginRight: '20px'}} >********</span>
-                  <span>
-                    <Link style={{ float: "right" }} href={`/reset-password`}>
-                      Change Password
-                    </Link>
-                  </span>
-                </div>
-              </Descriptions.Item>
+          <Descriptions.Item label="Password">
+            <div className={styles["user-profile-value"]}>
+              <span style={{ marginRight: "10px" }}>********</span>
+              <span>
+                <Link style={{ float: "right" }} href={`/reset-password`}>
+                  Change Password
+                </Link>
+              </span>
+            </div>
+          </Descriptions.Item>
 
-              <Descriptions.Item>
-                {!this.state.tgbtn ? (
-                  <React.Fragment>
-                    <Button
-                      onClick={() => this.toggleButton("cancel")}
-                      className="edit-icon"
-                      style={{ marginRight: "20px" }}
-                    >
-                      cancel
-                    </Button>
-                    <Button
-                      onClick={() => this.update()}
-                      className="edit-icon"
-                      type="primary"
-                    >
-                      Save
-                    </Button>
-                  </React.Fragment>
-                ) : null}
-              </Descriptions.Item>
-            </Descriptions>
-
-
+          <Descriptions.Item>
+            {!this.state.tgbtn ? (
+              <React.Fragment>
+                <Button
+                  onClick={() => this.toggleButton("cancel")}
+                  className="edit-icon"
+                  style={{ marginRight: "20px" }}
+                >
+                  cancel
+                </Button>
+                <Button
+                  onClick={() => this.update()}
+                  className="edit-icon"
+                  type="primary"
+                >
+                  Save
+                </Button>
+              </React.Fragment>
+            ) : null}
+          </Descriptions.Item>
+        </Descriptions>
       </Content>
     );
   }
