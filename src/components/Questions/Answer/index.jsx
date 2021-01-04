7138c3668a06312e0200039e12f66f0146c3e9b2
jitendra <jitendra.kumar@soprasteria.com>
2021-01-04 15:24:12 +0530

    test

--- src/components/Questions/Answer/index.jsx
+++ src/components/Questions/Answer/index.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { upperFirst, nth } from "lodash";
 import { Layout, Button, Modal } from "antd";
 import Link from "next/link";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
 import AppHead from "../../Head/head";
 import DataNoFound from "../../DataNoFound";
 import { getUser } from "../../../utils/index";
 import { httpPost, httpDelete } from "../../../utils/http";
 import { messageError, messageSuccess } from "../../../utils/antd";
-import {  EditOutlined,  DeleteOutlined,  ExclamationCircleOutlined} from "@ant-design/icons";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
 
-
-const Editor = dynamic(() => import('../../Editor'));
+const Editor = dynamic(() => import("../../Editor"));
 const { confirm } = Modal;
 const { Content } = Layout;
 
@@ -30,17 +33,26 @@ class Answer extends React.Component {
   constructor(props) {
     super(props);
 
-
     this.user = getUser();
     this.myRef = React.createRef();
-
-   this.state = {
-      _id: '',
+    this.state = {
+      _id: "",
       answer: "",
       loaded: false,
       data: props.answer,
     };
+  }
 
+  fetchLatestData() {
+    httpGet({ url: `bquestion/answer/${props._id}` })
+    .then((response) => {
+      this.setState({
+        data: response.result,
+      });
+    })
+    .catch((err) => {
+
+    });
   }
 
 
@@ -92,11 +104,11 @@ class Answer extends React.Component {
           answer: "",
         });
         // this.componentDidMount();
+        this.fetchLatestData();
         scrollToRefObject();
         messageSuccess({ content: "Your answer is saved successfully" });
       })
       .catch((err) => {
-
         messageError({ content: "something went wrong" });
       });
   }
@@ -139,6 +151,7 @@ class Answer extends React.Component {
         httpDelete({ url: `question/delete/${questionId}/${answerId}` })
           .then((response) => {
             // that.componentDidMount();
+            this.fetchLatestData();
             messageSuccess({ content: "Deleted successfully" });
           })
           .catch((err) => {
@@ -152,142 +165,132 @@ class Answer extends React.Component {
     return (
       <Content>
         <AppHead data={this.state.data} />
-            <div className="row">
-              <div style={{ width: "100%" }}>
-                <h2>{this.state.data.title}</h2>
-                <p
-                  dangerouslySetInnerHTML={{ __html: this.state.data.body }}
-                ></p>
-                {this.user && this.user._id === this.state.data.author ? (
-                  <div>
-                    <DeleteOutlined
-                      style={{
-                        color: "red",
-                        float: "right",
-                        padding: "10px",
-                      }}
-                      onClick={() => {
-                        this.deleteQuestion(this.state.data._id);
-                      }}
-                    />
-                    <Link href={`/questions/edit/${this.state.data._id}`}>
-                      <EditOutlined
-                        style={{
-                          float: "right",
-                          padding: "10px",
-                        }}
-                      />
-                    </Link>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-              <div style={{ marginRight: "20px" }}>
-                <span>
-                  <span className="ask-view">Posted On:</span>
-                  <span>{this.date(this.state.data.created_at)}</span>
-                </span>
-              </div>
+        <div className="row">
+          <div style={{ width: "100%" }}>
+            <h2>{this.state.data.title}</h2>
+            <div  dangerouslySetInnerHTML={{ __html: this.state.data.body }}></div >
+            {this.user && this.user._id === this.state.data.author ? (
               <div>
-                <span>
-                  <span className="ask-view"> Viewed: </span>
-                  <span>{this.state.data.visits} times </span>{" "}
-                </span>
+                <DeleteOutlined
+                  style={{
+                    color: "red",
+                    float: "right",
+                    padding: "10px",
+                  }}
+                  onClick={() => {
+                    this.deleteQuestion(this.state.data._id);
+                  }}
+                />
+                <Link href={`/questions/edit/${this.state.data._id}`}>
+                  <EditOutlined
+                    style={{
+                      float: "right",
+                      padding: "10px",
+                    }}
+                  />
+                </Link>
               </div>
+            ) : (
+              ""
+            )}
+          </div>
+          <div style={{ marginRight: "20px" }}>
+            <span>
+              <span className="ask-view">Posted On:</span>
+              <span>{this.date(this.state.data.created_at)}</span>
+            </span>
+          </div>
+          <div>
+            <span>
+              <span className="ask-view"> Viewed: </span>
+              <span>{this.state.data.visits} times </span>{" "}
+            </span>
+          </div>
 
-              {this.state.data.answer?.length ? (
-                this.state.data.answer.map((item, i) => {
-                  return (
-                    <div
-                      key={i}
-                      style={{
-                        marginTop: "25px",
-                        width: "100%",
-                      }}
-                    >
-                      <div className="listing border ">
-                        {this.user && this.user._id === item.ans_by?._id ? (
-                          <React.Fragment>
-                            <DeleteOutlined
-                              style={{
-                                color: "red",
-                                float: "right",
-                                padding: "10px",
-                              }}
-                              onClick={() => {
-                                this.deleteAnswer(
-                                  item._id,
-                                  this.state.data._id
-                                );
-                              }}
-                            />
-                            <EditOutlined
-                              style={{
-                                float: "right",
-                                padding: "10px",
-                              }}
-                              onClick={() => {
-                                this.editAnswer(item);
-                              }}
-                            />
-                          </React.Fragment>
-                        ) : (
-                          ""
-                        )}
-
-                        <div style={{ margin: "13px 0 0px 35px" }}>
-                          <p
-                            dangerouslySetInnerHTML={{ __html: item.answer }}
-                          ></p>
-                          <p
-                            style={{
-                              fontFamily: "monospace",
-                              fontStyle: "italic",
-                            }}
-                          >
-                            <span className="question-by">Answered By</span>{" "}
-                            {upperFirst(item.ans_by?.firstName)}
-                            <span
-                              className="question-by"
-                              style={{ marginLeft: "20px" }}
-                            >
-                              On
-                            </span>{" "}
-                            {this.date(item.added)}{" "}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })
-              ) : (
-                <div style={{ width: "100%", marginTop: "40px" }}>
-                  <DataNoFound data={{ text: "Not Answered Yet" }} />
-                </div>
-              )}
-              <div
-                ref={this.myRef}
-                style={{ marginTop: "70px", width: "100%" }}
-              >
-                <h5>Your Answer</h5>
-                <Editor
-                  data={this.state.answer}
-                  sendData={this.getEditorData.bind(this)}
-                />{" "}
-              </div>
-              <div style={{ marginTop: "30px" }}>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  onClick={this.saveAnswer.bind(this)}
-                  className="login-form-button"
-                  disabled={this.isButtonDisabled}
+          {this.state.data.answer?.length ? (
+            this.state.data.answer.map((item, i) => {
+              return (
+                <div
+                  key={i}
+                  style={{
+                    marginTop: "25px",
+                    width: "100%",
+                  }}
                 >
-                  Submit
-                </Button>
-              </div>
+                  <div className="listing border ">
+                    {this.user && this.user._id === item.ans_by?._id ? (
+                      <React.Fragment>
+                        <DeleteOutlined
+                          style={{
+                            color: "red",
+                            float: "right",
+                            padding: "10px",
+                          }}
+                          onClick={() => {
+                            this.deleteAnswer(item._id, this.state.data._id);
+                          }}
+                        />
+                        <EditOutlined
+                          style={{
+                            float: "right",
+                            padding: "10px",
+                          }}
+                          onClick={() => {
+                            this.editAnswer(item);
+                          }}
+                        />
+                      </React.Fragment>
+                    ) : (
+                      ""
+                    )}
+
+                    <div style={{ margin: "13px 0 0px 35px" }}>
+                      <div dangerouslySetInnerHTML={{ __html: item.answer }}></div>
+                      <p
+                        style={{
+                          fontFamily: "monospace",
+                          fontStyle: "italic",
+                        }}
+                      >
+                        <span className="question-by">Answered By</span>{" "}
+                        {upperFirst(item.ans_by?.firstName)}
+                        <span
+                          className="question-by"
+                          style={{ marginLeft: "20px" }}
+                        >
+                          On
+                        </span>{" "}
+                        {this.date(item.added)}{" "}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div style={{ width: "100%", marginTop: "40px" }}>
+              <DataNoFound data={{ text: "Not Answered Yet" }} />
             </div>
+          )}
+          <div ref={this.myRef} style={{ marginTop: "70px", width: "100%" }}>
+            <h5>Your Answer</h5>
+            <Editor
+              data={this.state.answer}
+              sendData={this.getEditorData.bind(this)}
+            />{" "}
+          </div>
+          <div style={{ marginTop: "30px" }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={this.saveAnswer.bind(this)}
+              className="login-form-button"
+              disabled={this.isButtonDisabled}
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
       </Content>
     );
   }
