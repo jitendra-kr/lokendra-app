c30423725c9a423dab96391649c58423c0b6ced5
jitendra <jitendra.kumar@soprasteria.com>
2021-02-19 21:39:57 +0530

    removed width from logs mai page

--- src/components/Blog/BlogList/index.jsx
+++ src/components/Blog/BlogList/index.jsx
@@ -40,7 +40,7 @@ class BlogList extends React.Component {
         <div className="row">
             <h1 style={{ fontFamily: "serif" }}>Trending</h1>
 
-          <div className="row" style={{width: '100%'}}>
+          <div className="row">
             {this.state.data.map((item, i) => {
               return (
                 <div
