613eb1ffdce8f5d7f703e83e4dc08d7f410ca71a
jitendra-kr <jitendrarajput588@gmail.com>
2022-05-03 20:08:03 +0530

    test copy to clip

--- src/components/StringToAscii/StringToAscii.jsx
+++ src/components/StringToAscii/StringToAscii.jsx
@@ -11,11 +11,18 @@ function StringToAscii() {
   const [input, setInput] = useState("");
   const [byte, setByte] = useState([]);
   const { url } = useGetUrl();
-  
+
 
   const onChange = ({ target: { value } }) => {
     setInput(value);
   };
+
+  const copyToClipboard = () => {
+    // document.execCommand()
+    navigator.clipboard.writeText("ll").then(() => {
+      alert("kkkk")
+    })
+  }
 
   const onClick = () => {
     const utf8Encode = new TextEncoder();
@@ -27,24 +34,31 @@ function StringToAscii() {
     <>
       <Content style={{ padding: "50px 50px" }}>
         <AppHead data={{
-           title: "convert string to ascii | text to ASCII converter - online string tools",
-            meta_description: "Useful, Simple and free online tool that converts string or plain text to ASCII codes in easy steps. Just type your text and it will convert to ASCII",
-            url }} />
+          title: "convert string to ascii | text to ASCII converter - online string tools",
+          meta_description: "Useful, Simple and free online tool that converts string or plain text to ASCII codes in easy steps. Just type your text and it will convert to ASCII",
+          url
+        }} />
+
         <div style={{
           display: "flex",
           "justify-content": "center",
         }}>
+          <div className="row" style={{ width: "100%", justifyContent: "center" }} >
 
-          <Form className={styles["input-width"]}>
+            <h1 style={{textAlign: "center", marginBottom: "30px"}}>
+              String to ASCII converter
+            </h1>
+ 
+            <Form className={styles["input-width"]}>
             <div >
-            <Form.Item
+              <Form.Item
                 name="title"
                 label=""
                 rules={[{ required: true, message: "Please enter text !" }]}
               >
- <Input className={styles["input"]} onChange={onChange} placeholder="Please enter" ></Input>
+                <Input className={styles["input"]} onChange={onChange} placeholder="Please enter" ></Input>
               </Form.Item>
-             
+
             </div>
             <div className={styles['button-div']} >
 
@@ -54,7 +68,7 @@ function StringToAscii() {
                 htmlType="submit"
                 style={{
                   height: "50px",
-                  marginTop: "30px",
+                  marginTop: "10px",
                   textAlign: "center"
                 }}
               >
@@ -62,10 +76,14 @@ function StringToAscii() {
               </Button>
             </div>
             <div>
-
-              {byte.length > 0 && <h3 style={{ textAlign: "center", marginTop: "30px", "word-wrap": "break-word" }} >[{byte.toString()}]</h3>}
+            {byte.length > 0 && <p onClick={copyToClipboard} className={styles.CopyToClipboardTxt}>Copy to clipboard</p>}
+            {byte.length > 0 && <div className={styles["ascii-div"]} >
+            <h3 className={styles.asciiCode} >[{byte.join(" ")}]</h3>
+                </div>}
             </div>
           </Form>
+          </div>
+
         </div>
       </Content>
     </>
