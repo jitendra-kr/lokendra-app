55ede5bb0765074067e95a65716c813e7f176b7f
jitendra-kr <jitendrarajput588@gmail.com>
2022-05-15 21:20:03 +0530

    feature : updated JSON viewer

---
+++ src/components/tools/CopyToClip/CopyToClip.tsx
@@ -0,0 +1,16 @@
+
+import styles from "./CopyToClip.module.css";
+
+export type CopyToClipProps = {
+    content: string;
+    copyToClipboardCb: () => void;
+    copyToText: string
+}
+export const CopyToClip = ({ content, copyToClipboardCb, copyToText }: CopyToClipProps) => {
+    console.log(typeof content === "object" || content.length > 0)
+    return <>
+        <div className={styles.container}>
+            {(typeof content === "object" || content.length > 0) && <p onClick={copyToClipboardCb} className={styles.CopyToClipboardTxt}>{copyToText}</p>}
+        </div>
+    </>
+}
