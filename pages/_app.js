f8d8a9bf790570052b53cb796243902859872e11
Jitendra-kr <jitendrarajput588@gmail.com>
2023-05-09 15:30:02 +0530

    disabled dynamic paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,40 @@
-import '../styles/global.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'antd/dist/antd.css';
+import "../styles/global.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "antd/dist/antd.css";
 
-import React, { useState } from 'react';
-import Router from 'next/router';
-import { Provider } from 'react-redux'
-import { store } from "../src/common/state"
+import React, { useState } from "react";
+import Router from "next/router";
+import { Provider } from "react-redux";
+import { store } from "../src/common/state";
 import axios from "axios";
 import { sample } from "lodash";
-import { LoadingOutlined } from '@ant-design/icons';
-import { UserContext } from '../src/contexts/UserContext'
-import { getUser } from '../src/utils'
-import { messageDestroy } from "../src/utils"
-import * as gtag from '../lib/gtag'
-import Config from '../src/config/env';
+import { LoadingOutlined } from "@ant-design/icons";
+import { UserContext } from "../src/contexts/UserContext";
+import { getUser } from "../src/utils";
+import { messageDestroy } from "../src/utils";
+import * as gtag from "../lib/gtag";
+import Config from "../src/config/env";
 import { Layout, Spin } from "antd";
+import { useWindowSize } from "../src/hooks/useWindowSize";
 
-import { MainHeader, MainFooter, AdComponent } from "../src/components"
+import { MainHeader, MainFooter, AdComponent } from "../src/components";
 
 const antIcon = <LoadingOutlined style={{ fontSize: 70 }} spin />;
 const baseUrls = Config.getData().default.baseUrl;
 
 export default function MyApp({ Component, pageProps }) {
-
-
   let isLoading = 0;
   const [loader, setLoader] = useState(false);
   const [user, setUser] = useState(getUser());
+  const size = useWindowSize();
 
   const startStopLoader = (value) => {
     setLoader(value);
-  }
+  };
 
-  axios.interceptors.request.use(config => {
+  axios.interceptors.request.use((config) => {
     config.baseURL = sample(baseUrls);
-    if (config.method === 'get') {
+    if (config.method === "get") {
       if (!loader) {
         startStopLoader(true);
         isLoading++;
@@ -43,22 +43,23 @@ export default function MyApp({ Component, pageProps }) {
     return config;
   });
 
-  axios.interceptors.response.use(config => {
-    if (loader || isLoading) {
-      startStopLoader(false);
-      isLoading--;
+  axios.interceptors.response.use(
+    (config) => {
+      if (loader || isLoading) {
+        startStopLoader(false);
+        isLoading--;
+      }
+      messageDestroy();
+      return config;
+    },
+    (err) => {
+      messageDestroy();
+      if (loader) {
+        startStopLoader(false);
+      }
+      return Promise.reject(err);
     }
-    messageDestroy();
-    return config;
-  }, (err) => {
-
-    messageDestroy();
-    if (loader) {
-      startStopLoader(false);
-    }
-    return Promise.reject(err);
-
-  });
+  );
 
   Router.onRouteChangeStart = () => {
     if (!loader) {
@@ -67,7 +68,7 @@ export default function MyApp({ Component, pageProps }) {
   };
 
   Router.onRouteChangeComplete = (url) => {
-    gtag.pageview(url)
+    gtag.pageview(url);
     if (loader) {
       startStopLoader(false);
     }
@@ -77,23 +78,25 @@ export default function MyApp({ Component, pageProps }) {
   };
 
   return (
-
     <Layout>
       <Spin indicator={antIcon} className="center-loader" spinning={loader} />
-      <Provider store={store} >      
-      <UserContext.Provider value={[user, setUser]}>
-        <MainHeader />
-        <div className="row">
-          <div className="col-lg-10" style={{ padding: "50px 0px 59px 30px" }}>
-            <Component {...pageProps} />
+      <Provider store={store}>
+        <UserContext.Provider value={[user, setUser]}>
+          <MainHeader />
+          <div className="row" style={{ height: size.height + "px" }}>
+            <div
+              className="col-lg-10"
+              style={{ padding: "50px 0px 59px 30px" }}
+            >
+              <Component {...pageProps} />
+            </div>
+            <div className="col-lg-2">
+              <AdComponent />
+            </div>
           </div>
-          <div className="col-lg-2" >
-            <AdComponent />
-          </div>
-        </div>
-      </UserContext.Provider>
+          <MainFooter />
+        </UserContext.Provider>
       </Provider>
-      <MainFooter />
     </Layout>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,16 @@ function HomePage({blog}) {
     </>
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-  const url = `${sample(baseUrls)}blog-management/blogs`;
-  let response: any = await fetch(url);
-  response = await response.json();
-  return {
-    props: {
-      blog: response.result
-    },
-    revalidate: 10
-  }
-}
+//   const url = `${sample(baseUrls)}blog-management/blogs`;
+//   let response: any = await fetch(url);
+//   response = await response.json();
+//   return {
+//     props: {
+//       blog: response.result
+//     },
+//     revalidate: 10
+//   }
+// }
 export default HomePage
