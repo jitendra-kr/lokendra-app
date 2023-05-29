54f42a2ea8381848a2807f9290e9e231f11a7373
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-05-29 19:38:05 +0530

    Update JsonViewer.tsx

--- src/components/tools/helper/JsonViewer/JsonViewer.tsx
+++ src/components/tools/helper/JsonViewer/JsonViewer.tsx
@@ -1,87 +1,85 @@
-import { JSONTree } from 'react-json-tree';
+import { JSONTree } from "react-json-tree";
 import styles from "./JsonViewer.module.css";
-import { JSONTreeTheme } from './editorTheme';
-import { get, isArray } from "lodash"
-import { ToolOutputActions } from '../ToolOutputActions';
-import { STRING_CONSTANTS } from '../../../../constants';
+import { JSONTreeTheme } from "./editorTheme";
+import { get, isArray } from "lodash";
+import { ToolOutputActions } from "../ToolOutputActions";
+import { STRING_CONSTANTS } from "../../../../constants";
 
 type JsonViewerProps = {
-    content: any;
-    error: any
-}
+  content: any;
+  input: string;
+  error: any;
+};
 
-export const JsonViewer = ({ content, error }: JsonViewerProps) => {    
+function RenderJsonViewer({ content, error, input }: JsonViewerProps) {
+  const valueColor: any = {
+    boolean: "boolean",
+    number: "number",
+    string: "string",
+  };
 
-    const valueColor: any = {
-        boolean: "boolean",
-        number: "number",
-        string: "string"
+  const typeOf = (value: any) => {
+    if (value === "true" || value === "false") {
+      return "boolean ";
+    } 
+    if( value === "null") {
+        return "object "
     }
+    return typeof value + " ";
+  };
 
-    const typeOf = (value: any) => {
-        if (value === "true" || value === "false") {
-            return "boolean"
-        }
-        return typeof value
-    }
+  if (!input) {
+    return <></>;
+  }
 
-    const isNativeArrayOrObj = (value: string) => {
-        return value === `"${STRING_CONSTANTS.tools.internalArray}"` || value === `"${STRING_CONSTANTS.tools.internalObject}"`
-    }
+  if (content && content === STRING_CONSTANTS.tools.invalidJson) {
+    return (
+      <span className={styles.invalidJson}>
+        {content} ----&gt; {get(error, "name") + " " + get(error, "message")}
+      </span>
+    );
+  }
 
-    const nativeArrayOrObj = (value: string) => {
-        if(value === `"${STRING_CONSTANTS.tools.internalArray}"`) {
-            return "Array"
-        } else if (value === `"${STRING_CONSTANTS.tools.internalObject}"`) {
-            return "Object"
-        } else {
-            return "Invalid value"
-        }
-    }
-    return <>
-        <ToolOutputActions content={content} />
-        <div className={styles.container} >
-            {
-                !content ? <></> :
-                    content === STRING_CONSTANTS.tools.invalidJson ?
-                        
-                        <span className={styles.invalidJson} >{content} ----&gt; {get(error, "name") + " " + get(error, "message")}
-                        
-                        </span> :
-                        <>
-                            {isArray(content) ? "[" : "{"}                           
-                            <JSONTree
-                                data={content}
-                                labelRenderer={([raw]) => <span className={styles.keyValues}>"{raw}":</span>}
-                                valueRenderer={(raw: any) => (
-                                    <em>
-                                        <span className={`${styles.keyValues} ${styles.dataTypeLabel}`}>
-                                            {!isNativeArrayOrObj(raw) ?  typeOf(raw) : ""}
-                                        </span>{' '}
-                                        <span className={`${styles.keyValues} ${styles[valueColor[typeOf(raw)]]}`}>
-                                            {!isNativeArrayOrObj(raw) ? raw : 
-                                            <>
-                                            <span className={styles.invalidValues} >
-                                               { nativeArrayOrObj(raw)}
-                                            </span>
-                                            <span className={styles.SyntaxError}> &#60;--------SyntaxError</span>
-                                            </>
-                                             }
-                                        </span>
-                                    </em>
-                                )}
-                                theme={{
-                                    extend: JSONTreeTheme,
-                                }}
-                                shouldExpandNode={() => true}
-
-                                hideRoot={true}
-                            />
-                            {isArray(content) ? "]" : "}"}
-                            {/* <ReactJson style={{ padding: 20 }} src={content} iconStyle={"square"} name={false} /> */}
-
-                        </>
-            }
-        </div>
+  return (
+    <>
+      {isArray(content) ? "[" : "{"}
+      <JSONTree
+        data={content}
+        labelRenderer={([raw]) => (
+          <span className={styles.keyValues}>"{raw}":</span>
+        )}
+        valueRenderer={(raw: any) => (
+          <em>
+            <span className={`${styles.keyValues} ${styles.dataTypeLabel}`}>
+              {typeOf(raw) }
+            </span>
+            <span
+              className={`${styles.keyValues} ${
+                styles[valueColor[typeOf(raw)]]
+              }`}
+            >
+              {raw}
+            </span>
+          </em>
+        )}
+        theme={{
+          extend: JSONTreeTheme,
+        }}
+        shouldExpandNode={() => true}
+        hideRoot={true}
+      />
+      {isArray(content) ? "]" : "}"}
     </>
+  );
 }
+
+export const JsonViewer = ({ content, error, input }: JsonViewerProps) => {
+  return (
+    <>
+      <ToolOutputActions content={content} />
+      <div className={styles.container}>
+        <RenderJsonViewer content={content} error={error} input={input} />
+      </div>
+    </>
+  );
+};
