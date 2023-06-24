4e7d41005e3cf240a3c5657638d9dc1de3076af1
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-06-24 12:15:25 +0530

    Add files via upload

---
+++ pages/uuid-generator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UUIDGenerator from "../../src/components/tools/UUIDGenerator/UUIDGenerator";
+
+function UUIDGeneratorPage() {
+  return <UUIDGenerator />;
+}
+
+export default UUIDGeneratorPage;
