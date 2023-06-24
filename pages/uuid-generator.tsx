ffaec2d75d1f72451e822d08d2e4f58c860bd110
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-06-24 12:18:42 +0530

    Delete uuid-generator.tsx

--- pages/uuid-generator.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UUIDGenerator from "../../src/components/tools/UUIDGenerator/UUIDGenerator";
-
-function UUIDGeneratorPage() {
-  return <UUIDGenerator />;
-}
-
-export default UUIDGeneratorPage;
