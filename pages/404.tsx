58c10b6e30a46470a7e135e190b90455293b2ab9
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-03 20:23:59 +0530

    updated 404 page and removed interceptors

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import { Button, Layout } from "antd";
+import Link from "next/link";
+import React from "react";
+import { STRING_CONSTANTS } from "../src/constants";
+
+const { Content } = Layout;
+
+function Page404() {
+  return (
+    <Content>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Page Not Found</h1>
+      <p style={{ textAlign: "center", marginTop: "50px", fontSize: "20px" }}>
+        Oops! We couldn't find what you were looking for, but we're here to
+        help. Let's get you back on track!
+      </p>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <Button type="primary" htmlType="submit">
+          <Link href={`/`} style={{ fontWeight: "bold" }}>
+            Explore {STRING_CONSTANTS.global.appName}`s tools
+          </Link>
+        </Button>
+      </div>
+    </Content>
+  );
+}
+
+export default Page404;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,20 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "../styles/global.css";
-
 import { LoadingOutlined } from "@ant-design/icons";
 import { Layout, Spin } from "antd";
-import axios from "axios";
-import { sample } from "lodash";
+import "bootstrap/dist/css/bootstrap.css";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import * as gtag from "../lib/gtag";
 import { store } from "../src/common/state";
-import Config from "../src/config/env";
+import { AdComponent, MainFooter, MainHeader } from "../src/components";
 import { UserContext } from "../src/contexts/UserContext";
 import { useWindowSize } from "../src/hooks/useWindowSize";
-import { getUser, messageDestroy } from "../src/utils";
-
-import { AdComponent, MainFooter, MainHeader } from "../src/components";
+import { getUser } from "../src/utils";
+import "../styles/global.css";
 
 const antIcon = <LoadingOutlined style={{ fontSize: 70 }} spin />;
-const baseUrls = Config.getData().default.baseUrl;
 
 export default function MyApp({ Component, pageProps }) {
-  let isLoading = 0;
   const [loader, setLoader] = useState(false);
   const [user, setUser] = useState(getUser());
   const size = useWindowSize();
@@ -30,35 +23,6 @@ export default function MyApp({ Component, pageProps }) {
   const startStopLoader = (value) => {
     setLoader(value);
   };
-
-  axios.interceptors.request.use((config) => {
-    config.baseURL = sample(baseUrls);
-    if (config.method === "get") {
-      if (!loader) {
-        startStopLoader(true);
-        isLoading++;
-      }
-    }
-    return config;
-  });
-
-  axios.interceptors.response.use(
-    (config) => {
-      if (loader || isLoading) {
-        startStopLoader(false);
-        isLoading--;
-      }
-      messageDestroy();
-      return config;
-    },
-    (err) => {
-      messageDestroy();
-      if (loader) {
-        startStopLoader(false);
-      }
-      return Promise.reject(err);
-    },
-  );
 
   useEffect(() => {
     router.events.on("routeChangeStart", (url, { shallow }) => {
