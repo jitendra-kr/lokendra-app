fe51bda652e9e4016c1e023057b71db317dcdc94
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-03 16:36:58 +0530

    Skeleton added on tool list page

--- src/components/tools/ToolsList/RenderToolsList.tsx
+++ src/components/tools/ToolsList/RenderToolsList.tsx
@@ -1,7 +1,27 @@
+import { Skeleton } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { NoMatchFound } from "./NoMatchFound";
 import { ITools } from "./toolsListingData";
+
+function LoadingToolsList() {
+  return (
+    <div className="row" style={{ minHeight: "40vh" }}>
+      {[...Array(12)].map(() => (
+        <div className="col-lg-4">
+          <div className="home-page-title" style={{ textAlign: "center" }}>
+            <Skeleton.Button
+              active={true}
+              size={"large"}
+              shape={"round"}
+              block={false}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export function RenderToolsList({
   toolsList,
@@ -17,26 +37,29 @@ export function RenderToolsList({
   };
 
   return (
-    <div className="row">
-      {toolsList.map((item, i) => {
-        return (
-          <div
-            className="col-lg-4 cursor-pointer"
-            key={i}
-            onClick={() => {
-              handleClick(item);
-            }}
-            style={{ marginTop: "25px" }}
-          >
-            <div className="listing border">
-              <div className="home-page-title" style={{ textAlign: "center" }}>
-                <Link href={item.link}>{item.title}</Link>
+    <>
+      <div className="row">
+        {toolsList.map((item, i) => {
+          return (
+            <div
+              className="col-lg-4 cursor-pointer"
+              key={i}
+              onClick={() => {
+                handleClick(item);
+              }}
+              style={{ marginTop: "25px" }}
+            >
+              <div className="listing border">
+                <div className="home-page-title text-align-center">
+                  <Link href={item.link}>{item.title}</Link>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-      {textInput && toolsList.length === 0 && <NoMatchFound />}
-    </div>
+          );
+        })}
+        {textInput && toolsList.length === 0 && <NoMatchFound />}
+      </div>
+      {!textInput && toolsList.length === 0 && <LoadingToolsList />}
+    </>
   );
 }
