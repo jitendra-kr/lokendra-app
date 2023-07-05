35f332743fae95ad940c7ec567c6442b3fd438f1
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-05 21:51:41 +0530

    styling updated for salary calculatopn tool

--- src/components/tools/SalaryHikePercentageCalculator/SalaryHikePercentageCalculator.tsx
+++ src/components/tools/SalaryHikePercentageCalculator/SalaryHikePercentageCalculator.tsx
@@ -1,9 +1,9 @@
 import { withRouter } from "next/router";
 import { ToolKeys } from "../ToolsList";
-
 import { InputOutputViewer } from "../helper/InputOutputViewer";
 import { HikeInPercentageBySalary } from "./HikeInPercentageBySalary";
 import { NewSalaryByPercentage } from "./NewSalaryByPercentage";
+import styles from "./SalaryHikePercentageCalculator.module.css";
 
 function SalaryHikePercentageCalculator() {
   return (
@@ -11,14 +11,14 @@ function SalaryHikePercentageCalculator() {
       toolId={ToolKeys.SalaryHike}
       byte={""}
       inputChild={
-        <div className="col-lg-4">
+        <div className={"col-lg-4 " + styles.container}>
           <HikeInPercentageBySalary />
         </div>
       }
       outputChild={
         <>
           <div className="col-lg-3"></div>
-          <div className="col-lg-4">
+          <div className={"col-lg-4 " + styles.container}>
             <NewSalaryByPercentage />
           </div>
         </>
