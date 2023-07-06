4d7ee92d9641a7a8d06b8ebd98506e95551c47f2
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-06 16:57:32 +0530

    use tsx for AdComponent

--- src/components/AdComponent/AdComponent.tsx
+++ src/components/AdComponent/AdComponent.tsx
@@ -5,7 +5,9 @@ export function AdComponent() {
     var ads = document.getElementsByClassName("adsbygoogle").length;
     for (var i = 0; i < ads; i++) {
       try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+          {},
+        );
       } catch (e) {}
     }
   }, []);
