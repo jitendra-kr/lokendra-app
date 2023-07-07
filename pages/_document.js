27643b3acb2c588037a43ea4ce31dadd2c14fb43
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-07 15:08:06 +0530

    Remaned crossorigin to crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4375413214168925"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <script
           dangerouslySetInnerHTML={{
