ae9ed3a3b4b4a1c63a2d86ac3dde32c42afd5db9
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-11 14:00:56 +0530

    updated _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,17 +19,19 @@ export default function Document() {
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4375413214168925`}
           crossOrigin="anonymous"
         />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-        </Script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+          }}
+        />
       </Head>
       <body>
         <Main />
