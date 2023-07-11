27d2d3ebcd05d61f2a00d93bdd9db18aced57fa8
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-07-11 13:41:17 +0530

    added gtag to next`s script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,27 +9,27 @@ export default function Document() {
     <Html>
       <Head>
         <Script
+          id="googletagmanager"
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         />
         <Script
+          id="adsbygoogle"
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4375413214168925`}
           crossOrigin="anonymous"
         />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-          }}
-        />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
