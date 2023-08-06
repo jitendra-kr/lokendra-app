74c36b9bc2760ea583f0cf2a4ce089decf7a1d5b
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-08-06 17:58:10 +0530

    updated reasons

--- src/components/tools/ToolsList/ToolsList.tsx
+++ src/components/tools/ToolsList/ToolsList.tsx
@@ -20,7 +20,7 @@ const Reasons = () => {
     `Data is processed locally rather than being sent to external servers.`,
     `It analyse and alter data significantly more quickly than other solutions on the market because to its strong algorithms and optimised speed.`,
     `Discover the power of precision with the unrivalled quality and dependability of ${STRING_CONSTANTS.global.domain}. Our tools is the best option regardless of whether you're a student, data analyst, business owner, or developer because of its superiority and dependability.`,
-    `Your data, in our opinion, is your most significant asset. We never use or sell your information to outside parties because of this. Therefore, why use tools that compromise your data?? Choose
+    `Our opinion is that your most valuable asset is your data. We never make use of or sell your information to third parties. Therefore, why use tools that compromise your data?? Choose
     ${STRING_CONSTANTS.global.domain} and take control of your data
     today.`,
   ];
