6b9316e170b8b52e020e2205e57911c40560dbd3
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2023-09-06 10:51:41 +0530

    In-Page Push (Banner) added

---
+++ public/monetagCdn/in-page-push-banner-anti-adblock/main.js
@@ -0,0 +1,6 @@
+(function (d, z, s) {
+    s.src = "https://" + d + "/400/" + z;
+    try {
+      (document.body || document.documentElement).appendChild(s);
+    } catch (e) {}
+  })("zoawufoy.net", 6298295, document.createElement("script"));
--- src/components/Google/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/Google/GoogleAnalytics/GoogleAnalytics.tsx
@@ -8,12 +8,25 @@ export function GoogleAnalytics() {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
+      {/* Start  in-page-push-banner*/}
       <script
+        id="moneyTag-in-page-push-banner-anti-adblock"
+        async
+        src={`../../../monetagCdn/in-page-push-banner-anti-adblock/anti-adblock.js`}
+      />
+      <script
+        id="moneyTag-in-page-push-banner-anti-adblock-main"
+        async
+        src={`../../../monetagCdn/in-page-push-banner-anti-adblock/main.js`}
+      />
+      {/* End  in-page-push-banner*/}
+
+      {/* <script
         id="adsbygoogle"
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4375413214168925`}
         crossOrigin="anonymous"
-      />
+      /> */}
 
       <script
         dangerouslySetInnerHTML={{
