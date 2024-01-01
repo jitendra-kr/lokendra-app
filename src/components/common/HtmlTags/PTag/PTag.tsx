f573c483f9d759d47923c8bd4dae80e6d47b7536
Jitendra-kr <jitendrarajput588@gmail.com>
2024-01-01 09:43:52 +0530

    added capibility to handle binary octal and hexa ASCII codes to convert to text

--- src/components/common/HtmlTags/PTag/PTag.tsx
+++ src/components/common/HtmlTags/PTag/PTag.tsx
@@ -1,5 +1,16 @@
 import PTagStyles from "./PTag.module.css";
 
-export function PTag({ text }: { text: string }) {
-  return <p className={PTagStyles.text}>{text}</p>;
+export function PTag({
+  text,
+  linkText,
+  linkUrl,
+}: {
+  text: string;
+  linkUrl?: string;
+  linkText?: string;
+}) {
+  const modifiedText = linkText
+    ? text.replace(linkText, `<a href="${linkUrl}">${linkText}</a>`)
+    : text;
+  return <p className={PTagStyles.text}>{modifiedText}</p>;
 }
