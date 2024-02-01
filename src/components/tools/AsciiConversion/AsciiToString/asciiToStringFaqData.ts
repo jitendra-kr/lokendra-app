a0e2467b07944ae5c97c67e9bb78456e10cce9c7
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2024-02-01 23:56:50 +0530

    docs: updated ascii to text content

--- src/components/tools/AsciiConversion/AsciiToString/asciiToStringFaqData.ts
+++ src/components/tools/AsciiConversion/AsciiToString/asciiToStringFaqData.ts
@@ -6,25 +6,28 @@ const asciiToStringFaqData: FaqProps[] = [
     ans: "A tool that turns ASCII codes back into the relevant characters is known as an ASCII-to-text converter.",
   },
   {
-    question: "How do I use the ASCII to Text Converter?",
-    ans: "To use the ASCII to Text Converter, simply input the ASCII codes into the provided field and system will automatically convert it to text.",
+    question: "How do I use this ASCII translator?",
+    ans: "Simply enter the ASCII codes into the provided field and system will automatically convert it to text.",
   },
   {
-    question: "Is using the ASCII to Text Converter free of charge?",
+    question: "Is using this tool free of charge?",
     ans: "Yes.",
   },
   {
-    question:
-      "Do I need to install anything to use the ASCII to Text Converter?",
+    question: "Do I need to install anything to use it?",
     ans: "No.",
   },
   {
-    question: "Is my data safe with the ASCII to Text Converter?",
-    ans: "Absolutely! We do not store or share any of the data you input into the ASCII to Text Converter.",
+    question: "Is my data safe with this ASCII converter?",
+    ans: "Absolutely! We do not store or share any of the data you input into the tool.",
   },
   {
-    question: "Can I convert a large amount of ASCII codes at once?",
+    question: "Can I decode a large amount of ASCII codes at once?",
     ans: "Yes",
+  },
+  {
+    question: "How can I download the converted text?",
+    ans: "Just click on download to save the result.",
   },
 ];
 
