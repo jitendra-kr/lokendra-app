7bac0fbf8e0e4f56e3940c3b86d3db2fddff503d
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2024-02-02 12:30:56 +0530

    docs: updated FAQ for stringToAsciiFaqData

--- src/components/tools/AsciiConversion/AsciiToString/asciiToStringFaqData.ts
+++ src/components/tools/AsciiConversion/AsciiToString/asciiToStringFaqData.ts
@@ -1,34 +1,38 @@
 import { FaqProps } from "../../../common";
 
-const asciiToStringFaqData: FaqProps[] = [
+const stringToAsciiFaqData: FaqProps[] = [
   {
-    question: "What is an ASCII to Text Converter?",
-    ans: "A tool that turns ASCII codes back into the relevant characters is known as an ASCII-to-text converter.",
+    question: "How does the Text to ASCII Converter work?",
+    ans: "It works by taking your input text and converting each character into its ASCII equivalent, providing a seamless and efficient conversion experience.",
   },
   {
-    question: "How do I use this ASCII translator?",
-    ans: "Simply enter the ASCII codes into the provided field and system will automatically convert it to text.",
-  },
-  {
-    question: "Is using this tool free of charge?",
-    ans: "Yes.",
-  },
-  {
-    question: "Do I need to install anything to use it?",
-    ans: "No.",
-  },
-  {
-    question: "Is my data safe with this ASCII converter?",
-    ans: "Absolutely! We do not store or share any of the data you input into the tool.",
-  },
-  {
-    question: "Can I decode a large amount of ASCII codes at once?",
+    question: "Is this tool free to use?",
     ans: "Yes",
   },
   {
-    question: "How can I download the converted text?",
-    ans: "Just click on download to save the result.",
+    question: "Do I need to install any software to use the it?",
+    ans: "No, our tool is a web-based tool, meaning you can use it directly in your browser without the need for any downloads or installations.",
+  },
+  {
+    question: "Is my data safe here?",
+    ans: "Absolutely! We prioritize your privacy. The Text to ASCII Converter does not store or share your data. It is used only for the conversion process and is immediately discarded afterwards.",
+  },
+  {
+    question: "Can I convert large amounts of text?",
+    ans: "Yes, our tool is designed to handle large amounts of text, making it perfect for both small and large conversion tasks.",
+  },
+  {
+    question: "Is ASCII still relevant in today's technology?",
+    ans: "Yes, ASCII is still widely used in various applications and is the foundation of modern computing.",
+  },
+  {
+    question: "How does ASCII differ from Unicode?",
+    ans: "ASCII uses 7 bits to represent each character, while Unicode uses 8, 16, or 32 bits, allowing it to represent a wider range of characters.",
+  },
+  {
+    question: "Can ASCII be used for images and videos?",
+    ans: "No, ASCII cannot directly represent images and videos. ASCII is limited to text data and cannot be used for non-text data such as images and videos.",
   },
 ];
 
-export default asciiToStringFaqData;
+export default stringToAsciiFaqData;
