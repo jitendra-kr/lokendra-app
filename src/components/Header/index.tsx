e510531ca86d22abcffd4730d9a16ecfe719e2d5
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2024-02-05 17:18:15 +0530

    fix: reset input on route change

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,28 +2,29 @@
 
 import styles from "@ft/styles/MainHeader.module.css";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import router from "next/router";
 import { useEffect } from "react";
 import { resetInput } from "../../common/state/tools";
 import { STRING_CONSTANTS } from "../../constants";
 import { useAppDispatch } from "../../hooks";
 
 function MainHeader() {
-  const router = useRouter();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const completeHandler = () => {
+    const handleRouteChange = () => {
+      // Dispatch reset action when the route changes
       dispatch(resetInput());
-      console.log("Router change completed");
     };
 
-    router.events.on("routeChangeComplete", completeHandler);
+    // Add event listener for route change
+    router.events.on("routeChangeComplete", handleRouteChange);
 
+    // Clean up the event listener on component unmount
     return () => {
-      router.events.off("routeChangeComplete", completeHandler);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [dispatch, router.events]);
+  }, [dispatch]);
 
   return (
     <header className={styles.header}>
