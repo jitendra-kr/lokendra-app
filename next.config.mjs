4e91f2ba1677107697e9228bd7e7d0a58d618761
Jitendra kumar rajput <jitendrarajput588@gmail.com>
2024-02-08 16:01:50 +0530

    chore: productionBrowserSourceMaps enabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     config.experiments = { asyncWebAssembly: true, layers: true };
