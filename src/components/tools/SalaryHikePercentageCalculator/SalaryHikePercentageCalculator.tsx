258677816a9b5c7fd25ef0d9399773c1bfcbfbe1
Jimmy <Jitendrarajput588@gmail.com>
2024-02-14 22:13:00 +0530

    fix: added faq and key features for salary calcular

--- src/components/tools/SalaryHikePercentageCalculator/SalaryHikePercentageCalculator.tsx
+++ src/components/tools/SalaryHikePercentageCalculator/SalaryHikePercentageCalculator.tsx
@@ -1,29 +1,34 @@
+import { Faq } from "@ft/components/common";
 import { ToolKeys } from "../ToolsList";
 import { InputOutputViewer } from "../helper/InputOutputViewer";
 import { HikeInPercentageBySalary } from "./HikeInPercentageBySalary";
 import { NewSalaryByPercentage } from "./NewSalaryByPercentage";
 import styles from "./SalaryHikePercentageCalculator.module.css";
+import SalaryHikePercentageCalculatorFaq from "./SalaryHikePercentageCalculatorFaq";
 
 function SalaryHikePercentageCalculator() {
   return (
-    <InputOutputViewer
-      toolId={ToolKeys.SalaryHike}
-      byte={""}
-      inputChild={
-        <>
-          <div className={styles.container}>
-            <HikeInPercentageBySalary />
-          </div>
-        </>
-      }
-      outputChild={
-        <>
-          <div className={styles.container}>
-            <NewSalaryByPercentage />
-          </div>
-        </>
-      }
-    />
+    <>
+      <InputOutputViewer
+        toolId={ToolKeys.SalaryHike}
+        byte={""}
+        inputChild={
+          <>
+            <div className={styles.container}>
+              <HikeInPercentageBySalary />
+            </div>
+          </>
+        }
+        outputChild={
+          <>
+            <div className={styles.container}>
+              <NewSalaryByPercentage />
+            </div>
+          </>
+        }
+      />
+      <Faq data={SalaryHikePercentageCalculatorFaq}></Faq>
+    </>
   );
 }
 
--- src/components/tools/ToolsList/toolsListingData.ts
+++ src/components/tools/ToolsList/toolsListingData.ts
@@ -506,6 +506,11 @@ Furthermore, our UUID generation tool is really simple to and use ensuring that 
       "Just enter your salary or Percentage and get your updated salary and the percentage increase with our salary hike calculator.",
     list: true,
     key: ToolKeys.SalaryHike,
+    keyFeatures: [
+      "The Online Salary Hike Percentage Calculator offers an interface that is easy to use.",
+      "The Online Salary Hike Percentage Calculator is built to be mobile-friendly, making it easy for individuals to access and use the tool on their smartphones or tablets.",
+      "With the help of the Online Salary Hike Percentage Calculator, salary hike calculations can be simplified.",
+    ],
     toolDescription: `The percentage change calculator is useful for figuring out how much an amount has increased or decreased in relative terms. It allows you to compare the significance of changes in various statistics, such as pay increases or drops.
     \nGet an accurate salary hike percentage calculation with our Salary Hike Percentage Calculator. Quickly enter your wage information and get a personalized salary hike percentage in just one click. Easily compare your wages against industry standards and ensure you're getting the raise you deserve.
     \nCertainly! An online salary hike percentage calculator is a free online tool that allows you to calculate the percentage increase in your salary after a raise or promotion. This tool is especially useful for employees who are negotiating a salary increase or trying to estimate their future earnings. 
