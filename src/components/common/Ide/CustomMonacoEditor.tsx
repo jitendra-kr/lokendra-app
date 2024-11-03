728d67a3c835e9f61de76a60003dc5795c2c1470
Jimmy <Jitendrarajput588@gmail.com>
2024-11-03 22:56:47 +0530

    chore: load monaco from cdn

--- src/components/common/Ide/CustomMonacoEditor.tsx
+++ src/components/common/Ide/CustomMonacoEditor.tsx
@@ -2,11 +2,11 @@ import { EditorProps } from "@monaco-editor/react";
 import dynamic from "next/dynamic";
 import LoadingMonaco from "./LoadingMonaco";
 
-import { loader } from "@monaco-editor/react";
+// import { loader } from "@monaco-editor/react";
 
-// https://github.com/suren-atoyan/monaco-react/issues/12
-// load monaco from node_modules
-loader.config({ paths: { vs: "../vs" } });
+// // https://github.com/suren-atoyan/monaco-react/issues/12
+// // load monaco from node_modules
+// loader.config({ paths: { vs: "../vs" } });
 
 const MonacoEditor = dynamic(() => import("@monaco-editor/react"), {
   ssr: false,
