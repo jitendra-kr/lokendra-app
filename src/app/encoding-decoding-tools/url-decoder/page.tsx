e44917974190abc06343454f0ede67490b7e8b68
Jimmy <Jitendrarajput588@gmail.com>
2024-11-24 21:56:24 +0530

    fix: URL encoding/decoding links

---
+++ src/app/encoding-decoding-tools/url-decoder/page.tsx
@@ -0,0 +1,17 @@
+import ToolRenderer from "@ft/components/ToolContentRenderer";
+import { ToolKeys } from "@ft/components/tools/ToolsList/ToolKeys";
+import URLDecode from "@ft/components/tools/URLDecode/URLDecode";
+import { generateMetaTags } from "@ft/seo/metaTags/generateMetaTags";
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
+
+export const metadata: Metadata = generateMetaTags(ToolKeys.UrlDecode);
+
+function URLDecoderPage() {
+  return (
+    <ToolRenderer toolKey={ToolKeys.UrlDecode}>
+      <URLDecode />
+    </ToolRenderer>
+  );
+}
+
+export default URLDecoderPage;
